f29397d2d58eea419c12b56c177aca31310c5cab
Dmitry <81821203+justTrueCodeWriter@users.noreply.github.com>
2023-07-25 14:27:53 +0300

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+set fish_greeting ""
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Aliases
+alias vim="nvim"
+alias nvimdiff="nvim -d"
+
+set PATH $PATH:~/.local/bin/
