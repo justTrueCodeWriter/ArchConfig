8e622e8a600c6b0fd199db1597a250a3e1ee1d14
justTrueCodeWriter <dmjustcoder8@gmail.com>
2024-10-13 00:22:57 +0300

    upd alias && plugins for nnn

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,7 @@ export EDITOR="nvim"
 
 # Aliases
 alias vv="nvim"
+alias nn="nnn"
 alias nvimdiff="nvim -d"
 alias emacs="emacs -nw"
 
@@ -18,6 +19,6 @@ source ~/.local/share/toolcat-comp
 set PATH $PATH:~/.local/bin/
 
 #NNN
-export NNN_PLUG='p:preview-tui;t:preview-tabbed'
+export NNN_PLUG='p:preview-tui;d:dragdrop'
 export NNN_USE_EDITOR=1
 set --export NNN_FIFO "/tmp/nnn.fifo"
