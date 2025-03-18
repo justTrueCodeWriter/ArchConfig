a99667ef6d8b89f9bdb954cda47dbcab9dd43304
justTrueCodeWriter <dmjustcoder8@gmail.com>
2025-03-19 00:16:02 +0300

    Fix autoconfiguration script

--- install.sh
+++ install.sh
@@ -1,19 +1,59 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Exec script with root, please"
+    exit 1
+fi
+
 echo "---HELLO AND WELCOME ABOARD---"
-echo "sxhkd & exec bspwm" > ~/.xinitrc;
-pacman -S xorg bspwm sxhkd sddm picom fish udisks2 i3lock polybar gcc nodejs npm rofi kitty neovim ranger htop firefox chromium obsidian fd flameshot sxiv tmux zathura syncthing;
 
-git clone https://github.com/justTrueCodeWriter/ArchConfig;
-mv ArchConfig/* ~./config/
+echo "sxhkd & exec bspwm" > ~/.xinitrc
 
-#--vimplug
+pacman -S --noconfirm \
+    xorg \
+    bspwm \
+    sxhkd \
+    sddm \
+    picom \
+    fish \
+    udisks2 \
+    i3lock \
+    polybar \
+    gcc \
+    nodejs \
+    npm \
+    rofi \
+    kitty \
+    neovim \
+    ranger \
+    htop \
+    firefox \
+    chromium \
+    obsidian \
+    fd \
+    flameshot \
+    sxiv \
+    tmux \
+    zathura \
+    syncthing
+
+git clone https://github.com/justTrueCodeWriter/ArchConfig
+mv ArchConfig/* ~/.config/
+
+# Vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-echo "Would you like setup hacker workspace? (y/n)";
-read ans;
+echo "Would you like to setup hacker workspace? (y/n)"
+read ans
 
-if [ $ans = "y" ]; then
-	pacman -S openvpn nmap aircrack-ng inetutils;
+if [ "$ans" = "y" ]; then
+    pacman -S --noconfirm \
+        openvpn \
+        nmap \
+        aircrack-ng \
+        inetutils
 fi
 
 echo "-----SETUP COMPLETE!-----"
+
